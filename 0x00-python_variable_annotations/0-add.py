9de411b1a6928941ff53d48e716216e0ce87749f
Waerumuthoga <waerumuthoga@gmail.com>
2024-10-11 04:33:53 +0000

    task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Defines a sum type-annotated function."""
+
+
+def add (a: float, b: float) -> float:
+    """Function that takes float arguments and returns float sum."""
+    return a + b
