ff2d932ec238f303d9a93dc4d6430bd7c6ee0352
Waerumuthoga <waerumuthoga@gmail.com>
2024-10-14 20:26:17 +0000

    task 4 correction

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
-"""Defines"""
-from typing import List
+"""4. Tasks"""
 import asyncio
+import random
+from typing import List
 
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """Function that takes code from wait_n and alters it to new function"""
-    tasks = await asyncio.gather(*tuple(map(lambda _:
-             task_wait_random(max_delay), range(n))))
-    return sorted(tasks)
+    """Take the code from wait_n and alter it into a new function task_wait_n.
+    The code is nearly identical to wait_n except task_wait_random is being
+    called."""
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    return [await task for task in asyncio.as_completed(tasks)]
